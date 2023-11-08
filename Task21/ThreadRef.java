bbbb409e35be59971583b0c0259d9a43b8d9efa6
Haswanth112 <147820442+Haswanth112@users.noreply.github.com>
2023-11-08 15:55:36 +0530

    Update ThreadRef.java

--- Task21/ThreadRef.java
+++ Task21/ThreadRef.java
@@ -1,9 +1,21 @@
-
 public class ThreadRef {
     public static void main(String[] args) {
-        System.out.println("Name: "+Thread.currentThread().getName());
-        System.out.println("Thread Id: "+Thread.currentThread().threadId());
-        System.out.println("State: "+Thread.currentThread().getState());
-        System.out.println("Priority: "+Thread.currentThread().getPriority());
+        Thread firstThread = new Thread(new MyThread(), "FirstThread");
+        Thread secondThread = new Thread(new MyThread(), "SecondThread");
+        firstThread.start();
+        secondThread.start();
+        System.out.println("Main Thread Name: " + Thread.currentThread().getName());
+        System.out.println("Main Thread Id: " + Thread.currentThread().getId());
+        System.out.println("Main Thread State: " + Thread.currentThread().getState());
+        System.out.println("Main Thread Priority: " + Thread.currentThread().getPriority());
+    }
+    static class MyThread extends Thread{
+        @Override
+        public void run() {
+            System.out.println("Name: " + Thread.currentThread().getName());
+            System.out.println(Thread.currentThread().getName()+" Thread Id: " + Thread.currentThread().getId());
+            System.out.println(Thread.currentThread().getName()+" State: " + Thread.currentThread().getState());
+            System.out.println(Thread.currentThread().getName()+" Priority: " + Thread.currentThread().getPriority());
+        }
     }
 }
