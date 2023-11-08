15d9d189fe2b957a4560c68c867f02276b028fd5
Haswanth112 <147820442+Haswanth112@users.noreply.github.com>
2023-11-08 17:33:19 +0530

    Update MultiThread.java

--- Task23/MultiThread.java
+++ Task23/MultiThread.java
@@ -4,16 +4,17 @@ class DisplayMessage extends Thread {
         this.Message=Message;
     }
     public void run() {
-        System.out.println(Message);
+        System.out.print(Message);
     }
 }
 
 public class MultiThread{
-    public static void main(String[] args) {
-        DisplayMessage thread1 = new DisplayMessage("Java is object-oriented, open source");
+    public static void main(String[] args) throws InterruptedException {
+        DisplayMessage thread1 = new DisplayMessage("Java is object-oriented, open source ");
         DisplayMessage thread2 = new DisplayMessage("easy to learn, high performance and fast.");
 
         thread1.start();
+        thread1.join();
         thread2.start();
     }
 }
