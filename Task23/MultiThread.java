f4e6540ceebd9b26ef8c6afdefb753798d572645
Haswanth112 <147820442+Haswanth112@users.noreply.github.com>
2023-11-15 15:33:06 +0530

    Update MultiThread.java

--- Task23/MultiThread.java
+++ Task23/MultiThread.java
@@ -1,21 +1,36 @@
-class DisplayMessage extends Thread {
-    private String Message;
-    public DisplayMessage(String Message){
-        this.Message=Message;
+class MyThread1 extends Thread {
+    private String[] words;
+
+    public MyThread1(String[] words) {
+        this.words = words;
     }
+
+    @Override
     public void run() {
-        System.out.print(Message);
+        for (String word : words) {
+            System.out.println(Thread.currentThread().getName() + ": " + word);
+            try {
+                Thread.sleep(500); // Sleep for 500 milliseconds
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " is dead.");
     }
 }
 
-public class MultiThread{
-    public static void main(String[] args) throws InterruptedException {
-        DisplayMessage thread1 = new DisplayMessage("Java is object-oriented, open source ");
-        DisplayMessage thread2 = new DisplayMessage("easy to learn, high performance and fast.");
+public class MultiThread {
+    public static void main(String[] args) {
+        String[] thread1Words = {"Java", "high", "performance", "and", "fast."};
+        String[] thread2Words = {"is", "object-oriented,", "open source,", "easy to learn,", "and"};
+
+        MyThread1 thread1 = new MyThread1(thread1Words);
+        MyThread1 thread2 = new MyThread1(thread2Words);
+
+        thread1.setName("Thread1");
+        thread2.setName("Thread2");
 
         thread1.start();
-        thread1.join();
         thread2.start();
     }
 }
-
