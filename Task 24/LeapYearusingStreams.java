92cde85298a3a4bc166de2645ed5cc4f23e6a669
Haswanth112 <147820442+Haswanth112@users.noreply.github.com>
2023-11-20 10:39:54 +0530

    Update LeapYearusingStreams.java

--- Task 24/LeapYearusingStreams.java
+++ Task 24/LeapYearusingStreams.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class LeapYearUsingStreams {
     public static void main(String[] args) {
         List<Integer> list=new ArrayList<>();
-        list.addAll(Arrays.asList(1998,2000,2001,2003,2003,2004,2005,2006,2006,2007,2010,2008,2008));
-        list.stream().filter(a->a%4==0 && a%100!=0 || (a%100==0 && a%400==0)).distinct().forEach(System.out::println);
+        Scanner s =new Scanner(System.in);
+        System.out.println("Enter the Years Separated by comma :");
+        String[] b=s.nextLine().split(",");
+        Arrays.stream(b).forEach(n->{list.add(Integer.parseInt(n));});
+        list.stream().
+                filter(a->a%4==0 && a%100!=0 || (a%100==0 && a%400==0)).
+                distinct().
+                forEach(System.out::println);
     }
 }
