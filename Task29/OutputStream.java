f36ce70facec907757071958316a25f76b2ff2fb
Haswanth112 <147820442+Haswanth112@users.noreply.github.com>
2023-11-22 11:13:14 +0530

    Update OutputStream.java

--- Task29/OutputStream.java
+++ Task29/OutputStream.java
@@ -2,15 +2,20 @@ import java.io.*;
 import java.util.Arrays;
 
 public class OutputStreamExample{
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) {
         File file=new File("E:\\TestFile.Txt");
-        file.createNewFile();
-        FileOutputStream out=new FileOutputStream(file);
-        out.write(24);
-        out.write(20);
-        out.write(30);
-        out.close();
-        System.out.println("Data added to file");
+        try {
+            file.createNewFile();
+            FileOutputStream out=new FileOutputStream(file);
+            out.write(24);
+            out.write(20);
+            out.write(30);
+            out.close();
+            System.out.println("Data added to file");
+        } catch (IOException e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+       
      // FileInputStream obj=new FileInputStream(file);
 
         //System.out.println(Arrays.toString(obj.readAllBytes()));
