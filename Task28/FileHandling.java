5fec002f252784555b0638bcd7c5f2f640adcfd4
Haswanth112 <147820442+Haswanth112@users.noreply.github.com>
2023-11-22 11:07:09 +0530

    Update FileHandling.java

--- Task28/FileHandling.java
+++ Task28/FileHandling.java
@@ -2,15 +2,24 @@
 import java.io.File;
 import java.io.IOException;
 
-public class FileHandling
-{
+public class FileHandling {
     public static void main(String args[]) throws IOException {
-        File folder=new File("D:\\TestFiles");
-        System.out.println(folder.mkdir());
-        File file=new File("D:\\TestFiles\\Sample.Txt");
-        System.out.println(file.createNewFile());
-       // file.delete();
-       //folder.delete();
+        File folder = new File("D:\\TestFiles");
+        if (folder.exists()) {
+            System.out.println("Folder Already exist");
+        } else {
+            System.out.println(folder.mkdir());
+            System.out.println("Folder Created");
+        }
+        File file = new File("D:\\TestFiles\\Sample.Txt");
+        if (file.exists()) {
+            System.out.println("File Already Exists");
+        } else {
+            System.out.println(file.createNewFile());
+            System.out.println("File Created");
+        }
 
+        file.delete();
+        folder.delete();
     }
 }
